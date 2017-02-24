fde4232435524c734d17e23ab28bad7697a5e4da
Jason Jalufka <jrj105@txstate.edu>
2017-02-24 11:46:55 -0600

    Add base shell scripts

---
+++ menu.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Display the main menu for contacts database management
+
+echo 'Welcome to my contact database, please select in the following menu:'
+echo '(a) Find a new record'
+echo '(b) Add a new record'
+echo '(c) Update a record'
+echo '(d) Remove a record'
+echo '(e) Quit'
