5afbfcc494c0141af6ed826c4ea689665c4a4111
Jason Jalufka <jrj105@txstate.edu>
2017-02-24 12:20:54 -0600

    Add user input

--- menu.sh
+++ menu.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 # Display the main menu for contacts database management
 
-echo 'Welcome to my contact database, please select in the following menu:'
-echo '(a) Find a new record'
-echo '(b) Add a new record'
-echo '(c) Update a record'
-echo '(d) Remove a record'
-echo '(e) Quit'
+echo 'Welcome to my contact database, please select in the following menu:
+(a) Find a new record
+(b) Add a new record
+(c) Update a record
+(d) Remove a record
+(e) Quit'
+
+read -r -p "Selection is: " selection
+
+case $selection in
+  [aA] ) echo "You chose a!" ;;
+  [bB] ) echo "You chose b!"
+    # call add.sh script
+    # echo "Jason:205 Nueces:1234567:jrj105@txstate.edu" >> 'database.txt' ;;
+  [cC] ) echo "You chose c!" ;;
+    
+  [dD] ) echo "You chose d!" ;;
+  [eE] ) echo "You chose e!" ;;
+  * ) "No match for selection" ;;
+esac
+
+cat 'database.txt'
