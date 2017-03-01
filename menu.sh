251df66958064394a86dad071f36d86d816a047a
matthewcano <swguitarest@yahoo.com>
2017-03-01 16:30:07 -0600

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 # Display the main menu for contacts database management
 
-source ./add.sh
-source ./find.sh
-source ./remove.sh
-source ./update.sh
-source ./interface.sh
-source ./utilities.sh
+#source ./add.sh
+#source ./find.sh
+#source ./remove.sh
+#source ./update.sh
+#source ./interface.sh
+#source ./utilities.sh
 
-displaymenu
-
+printf '\n'
+echo "a)find a record"
+echo "b)add a record"
+echo "c)update a record"
+echo "d)remove a record"
+echo "e)print all records"
+echo "f)print all deleted records"
+echo "g)quit"
+printf '\n'
+echo "Please Make a Selection"
+read selection
 case $selection in
-  [aA] ) echo "You chose a!" ;;
-  [bB] ) echo "You chose b!"
-    # call add.sh script
-    . ./add.sh ;;
-    # echo "Jason:205 Nueces:1234567:jrj105@txstate.edu" >> 'database.txt' ;;
-  [cC] ) echo "You chose c!" ;;
-    
-  [dD] ) echo "You chose d!" ;;
-  [eE] ) echo "You chose e!" ;;
-  * ) "No match for selection" ;;
+  [aA1] ) echo "You chose a!" ;;
+  [bB2] ) ./add.sh ;;
+  [cC3] ) echo "You chose c!" ;;
+  [dD4] ) ./remove.sh ;;
+  [eE5] ) cat 'database.txt' ;;
+  [fF6] ) cat 'deleted.txt' ;;
+  [gG7] ) echo "thank you for using our program! Goodbye!" exit ;;
 esac
-
-cat 'database.txt'
+~             
