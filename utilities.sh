44ecc5f743f743e55c237119b023535f328a72b3
Jason Jalufka <jrj105@txstate.edu>
2017-03-03 11:15:36 -0600

    Modify function definition style and add function to process user selection

--- utilities.sh
+++ utilities.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 # Various utilities to help with database management
 
-
-function loadArray {
+loadArray() {
+  echo "Loading Array!"
   while IFS= read -r line; do
     contacts+=("$line");
-  done < "database.txt"
-  
+  done < "database.txt" 
   echo "Done loading array!";
-
 }
 
-function printArray {
+printArray() {
+  echo "Printing contacts database:"
   for i in $contacts; do
     echo $i;
   done
+  echo "Done printing contacts database!"
+}
+
+processSelection() {
+  case "$1" in
+    [aA1] ) echo "You chose a (FIND)" ;;
+    [bB2] ) echo "You chose b (ADD)" ;;
+    [cC3] ) echo "You chose c (UPDATE)" ;;
+    [dD4] ) echo "You chose d (REMOVE)" ;;
+    [eE5] ) cat 'database.txt' ;;
+    [fF6] ) cat 'deleted.txt' ;;
+    [gG7] ) echo "Thank you. Goodbye" exit ;;
+  esac  
 }
