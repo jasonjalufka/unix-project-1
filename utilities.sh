7d5a3dfd01f3c175903d1f141df0bc4b338e177b
Jason Jalufka <jrj105@txstate.edu>
2017-03-06 00:40:42 -0600

    Move processSelection to interface.sh

--- utilities.sh
+++ utilities.sh
@@ -8,23 +8,7 @@ loadArray() {
   }
 
 printArray() {
-  printf "%s\n" "${contactsArray[@]}"
-}
-
-processSelection() {
-  case "$1" in
-    [aA1] ) echo "You chose a (FIND)"
-              processFind ;;
-    [bB2] ) echo "You chose b (ADD)"
-              ./add.sh ;;
-    [cC3] ) echo "You chose c (UPDATE)" 
-              ./update.sh ;;
-    [dD4] ) echo "You chose d (REMOVE)"
-              ./remove.sh ;;
-    [eE5] ) cat 'database.txt' ;;
-    [fF6] ) cat 'deleted.txt' ;;
-    [gG7] ) echo "Thank you. Goodbye" 
-              return 1 ;; #exit main menu loop
-  esac  
-  return 0; #continue looping main menu
+  header="Contact Database (Name:Address:Phone:Email)"
+  divider="-------------------------------------------"
+  printf "%s\n%s\n%s\n" "$header" "$divider" "${contactsArray[@]}"
 }
