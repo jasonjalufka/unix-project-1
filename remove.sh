a7136960f26c81d24fc4210c27c032248a946cdf
Jason Jalufka <jrj105@txstate.edu>
2017-03-06 11:18:03 -0600

    Push remove file for Matt to work on

--- remove.sh
+++ remove.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 # Removes an entry from the database
 
-printf '\n'
-echo "Whose records would you like to remove?"
-read delete
-cat database.txt | grep ${delete} >> deleted.txt
-sed -i /${delete}/d 'database.txt'
+#printf '\n'
+#echo "Whose records would you like to remove?"
+#read delete
+#cat database.txt | grep ${delete} >> deleted.txt
+#sed -i /${delete}/d 'database.txt'
+
+removeRecord() {
+  findRecord remove #searches for record to remove, passes "remove" operation flag
+}
+
+processRemove() {
+  index="$1"
+  
+  while [ "$exit" -ne 1 ]; do
+    read -r -p "Are you sure you wish to remove the following record?
+    "${contactsArray[$index]}"" confirm 
+    case "$confirm" in
+      [yY] ) printf "\nRemoving contact record:" 
+  done
+}
