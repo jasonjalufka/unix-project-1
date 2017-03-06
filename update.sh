400f289a93ec69e7873bea1381c03715fd6cb416
sylchristian <c_s323txstate.edu>
2017-03-06 12:11:14 -0600

    Pushing updated update.sh file but it still contains an error

--- update.sh
+++ update.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 # Allows the user to update an entry in the database
 
-echo "Whose records need to be updated?"
-read update
-if grep -Fq ${update} database.txt
-  then
-    sed -i /${update}/d 'database.txt'
-    ./add.sh
-else
-  echo "No match found. Note the database is case sensitive."
-fi
+#echo "Whose records need to be updated?"
+#read update
+#if grep -Fq ${update} database.txt
+#  then
+#    sed -i /${update}/d 'database.txt'
+#    ./add.sh
+#else
+#  echo "No match found. Note the database is case sensitive."
+#fi
+
+updateRecord() {
+  findRecord update
+}
+
+processUpdate() {
+  index="$1"
+
+  printf "%s\nPlease enter the information you wish to update: "
+  read -r -p "Name: " name
+  read -r -p "Address: " address
+  read -r -p "Phone: " phone
+  read -r -p "Email: " email
+  newRecord=""$name":"$address":"$phone":"$email""
+  "${contactsArray[$index]}"=("$newRecord")
+  clear
+  printf "Successfully updated the contact:\n%s\n" "$newRecod"
+  mainSelection
+}
